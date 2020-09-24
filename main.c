558160b074fb972c1f9e4f4d8f415c9e4f3a0af0
Pratyush219 <53852042+Pratyush219@users.noreply.github.com>
2020-09-24 17:35:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 void display_menu();
 void get_input(int**,int*);
-void display_array(int**, int size);
+void display_array(int*, int size);
 void insert_element(int**,int*);
-void delete_element(int**,int*);
+void delete_element(int*,int*);
 int main()
 {
 	int *arr;
@@ -20,7 +20,7 @@ int main()
 			case 1: get_input(&arr,&n);
 					break;
 			case 2: 
-						display_array(&arr,n);
+						display_array(arr,n);
 //						printf("No elements to display-ARRAY IS EMPTY\n");
 //					
 //						printf("The array elements are:\n");
@@ -31,7 +31,7 @@ int main()
 					break;
 			case 3: insert_element(&arr, &n);
 					break;
-			case 4: delete_element(&arr,&n);
+			case 4: delete_element(arr,&n);
 					break;
 			case 5: printf("Good bye...\n");
 					break;
@@ -44,52 +44,53 @@ int main()
 void display_menu(){
 	printf("1. Create an array\t2. Display array\t3. Insert\t4. Delete\t5. Exit\n");
 }
-void display_array(int **arr, int size){
+void display_array(int *arr, int size){
 	if(size==0)
 		printf("No elements to display-ARRAY IS EMPTY\n");
 	else{
 		printf("The array elements are:\n");
 		for(int i=0;i<size;++i)
-			printf("%d\t",*(arr)[i]);
+			printf("%d\t",arr[i]);
 		printf("\n");
 	}
 }
 void get_input(int **a, int* n){
 	printf("Enter number of elements: ");
 	scanf("%d",n);
-	*a=(int*)malloc((*n)*sizeof(int));
+	*a=(int*)calloc(*n,sizeof(int));
 		printf("Enter array elements: ");
 		for(int i=0;i<*n;++i)
 			scanf("%d",&(*a)[i]);
 }
 void insert_element(int **a,int *n){
-	*a=(int*)malloc(sizeof(int));
 	int idx, e=0;
 	printf("Enter the position at which you want to insert the element: ");
 	scanf("%d",&idx);
 	if(idx<1 || idx>*n+1)
 		printf("Invalid input- position must be from 1 to %d\n",(*n)+1);
 	else{
+		*a=(int*)realloc(*a,(*(n)+1)*sizeof(int));
 		printf("Enter the element to be inserted: ");
 		scanf("%d",&e);
 		for(int i=*n;i>idx-1;i--)
-			*a[i]=*a[i-1];
-		*a[idx-1]=e;
+			*(a)[i]=*(a)[i-1];
+		*(a)[idx-1]=e;
 		(*n)++;
 		printf("Element inserted\n");
 	}
 
 }
-void delete_element(int **a,int *n){
+void delete_element(int *a,int *n){
 	int idx;
 	printf("Enter the position from which element has to be deleted: ");
 	scanf("%d",&idx);
 	if(idx<1 || idx>*n)
+	
 		printf("Invalid input- position must be from 1 to %d\n",(*n));
 	else{
 		for(int i=idx-1;i<(*n)-1;i++)
-			*a[i]=*a[i+1];
-		*a[*n-1]=0;
+			a[i]=a[i+1];
+		a[*n-1]=0;
 		(*n)--;
 	}
 }
